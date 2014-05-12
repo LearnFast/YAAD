580c0381613326cf820ba673128695949e74e0d9
Karan Toor <karanbirsinghtoor@gmail.com>
2014-05-11 17:30:27 -0700

    built a form to create and view concepts

--- app/controllers/concepts_controller.rb
+++ app/controllers/concepts_controller.rb
@@ -1,2 +1,24 @@
 class ConceptsController < ApplicationController
+  def new
+    @concept = Concept.new
+  end
+
+  def create
+    @concept = Concept.new(concept_params)
+    if @concept.save
+      redirect_to @concept
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @concept = Concept.find(params[:id])
+  end
+
+  private
+
+  def concept_params
+    params.require(:concept).permit(:question, :answer)
+  end
 end
