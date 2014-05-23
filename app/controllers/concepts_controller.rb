e6bfe68ba4a92ff7fb410c8048c28d70e17086b8
Matt Schartman <matt.schartman@appfolio.com>
2014-05-22 22:42:01 -0700

    Makes user concepts work

--- app/controllers/concepts_controller.rb
+++ app/controllers/concepts_controller.rb
@@ -1,25 +1,14 @@
 class ConceptsController < ApplicationController
-  def new
-    @concept = Concept.new
-  end
-
   def create
-    @concept = Concept.create!(question: params[:question], answer: params[:answer])
+    concept = Concept.create!(question: params[:question], answer: params[:answer])
+    @user_concept = UserConcept.create!(user: current_user, concept: concept)
     respond_to do |format|
-      format.json { render json: @concept.id }
+      format.json { render json: @user_concept.id }
     end
   end
 
-  def show
-    @concept = Concept.find(params[:id])
-  end
-
-  def display_concepts
-    @concepts_ary = Concept.last(10)
-  end
-
   def destroy
-    Concept.find(params[:id]).destroy
+    UserConcept.find(params[:id]).destroy
     respond_to do |format|
       format.json { head :ok }
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140509034628) do
+ActiveRecord::Schema.define(version: 20140520022429) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 20140509034628) do
   end
 
   create_table "user_concepts", force: true do |t|
+    t.integer "user_id"
+    t.integer "concept_id"
   end
 
   create_table "users", force: true do |t|
