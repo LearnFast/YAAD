27d583193851b26ea1b4025a30384b1d6f2eb431
Karan Toor <karanbirsinghtoor@gmail.com>
2014-05-25 11:02:52 -0700

    made the controller code sexier

--- app/controllers/concepts_controller.rb
+++ app/controllers/concepts_controller.rb
@@ -1,7 +1,6 @@
 class ConceptsController < ApplicationController
   def create
-    current_user.concepts << Concept.create!(question: params[:question], answer: params[:answer])
-    @concept = Concept.last
+    @concept = Concept.create!(question: params[:question], answer: params[:answer]).tap{current_user.concepts << c}
     respond_to do |format|
       format.json { render json: @user_concept.id }
     end
@@ -12,11 +11,5 @@ class ConceptsController < ApplicationController
     respond_to do |format|
       format.json { head :ok }
     end
-  end
-
-  private
-
-  def concept_params
-    params.require(:concept).permit(:question, :answer)
   end
 end
