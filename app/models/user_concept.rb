a97909370efa77dcc857c9484dd4dbde89d2be67
Karan Toor <karanbirsinghtoor@gmail.com>
2014-05-27 22:48:14 -0700

    cant seem to update model attritubes inside models public method, user_concept

--- app/models/user_concept.rb
+++ app/models/user_concept.rb
@@ -2,16 +2,37 @@
 #
 # Table name: user_concepts
 #
-#  id          :integer          not null, primary key
-#  user_id     :integer
-#  concept_id  :integer
-#  attempt_id  :integer
-#  review_date :datetime
+#  id           :integer          not null, primary key
+#  user_id      :integer
+#  concept_id   :integer
+#  review_date  :datetime
+#  e_factor     :decimal(, )      default(2.5)
+#  rep_interval :integer          default(1)
 #
 
 class UserConcept < ActiveRecord::Base
   belongs_to :user
   belongs_to :concept
-  belongs_to :attempts
+  has_many :attempts
   validates_presence_of :review_date
+
+  def update_from_review! response_quality
+    self.e_factor     = new_e_factor response_quality
+    self.rep_interval = new_rep_interval
+    self.review_date  = Date.today + rep_interval
+    save!
+  end
+
+  private
+
+  def new_e_factor q
+    new_ef = e_factor + (0.1-(5-q)*(0.08+(5-q)*0.02))
+    new_ef < 1.3 ? 1.3 : new_ef
+  end
+
+  def new_rep_interval
+    return 1 if attempts.size == 1
+    return 6 if attempts.size == 2
+    (rep_interval * e_factor).ceil
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140520022429) do
+ActiveRecord::Schema.define(version: 20140528051620) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,9 +20,11 @@ ActiveRecord::Schema.define(version: 20140520022429) do
     t.datetime "updated_at"
     t.string   "answer"
     t.integer  "previous_id"
+    t.integer  "user_concept_id"
   end
 
   add_index "attempts", ["previous_id"], name: "index_attempts_on_previous_id", using: :btree
+  add_index "attempts", ["user_concept_id"], name: "index_attempts_on_user_concept_id", using: :btree
 
   create_table "concepts", force: true do |t|
     t.string "question"
@@ -32,11 +34,11 @@ ActiveRecord::Schema.define(version: 20140520022429) do
   create_table "user_concepts", force: true do |t|
     t.integer  "user_id"
     t.integer  "concept_id"
-    t.integer  "attempt_id"
     t.datetime "review_date"
+    t.decimal  "e_factor",     default: 2.5
+    t.integer  "rep_interval", default: 1
   end
 
-  add_index "user_concepts", ["attempt_id"], name: "index_user_concepts_on_attempt_id", using: :btree
   add_index "user_concepts", ["concept_id"], name: "index_user_concepts_on_concept_id", using: :btree
   add_index "user_concepts", ["user_id"], name: "index_user_concepts_on_user_id", using: :btree
 
