e832309489c3d5f719ac8882530d1da129f3a3c5
Matt Schartman <matt.schartman@appfolio.com>
2014-06-03 22:04:27 -0700

    Deleting unused stuff

--- app/controllers/concepts_controller.rb
+++ app/controllers/concepts_controller.rb
@@ -14,9 +14,4 @@ class ConceptsController < ApplicationController
       format.json { head :ok }
     end
   end
-
-  def update
-    UserConcept.find_by(user: current_user, concept_id: params[:id]).update_from_review! params[:response_quality].to_i
-    head :no_content
-  end
 end
