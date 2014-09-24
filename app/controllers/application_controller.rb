16e5eb3d95da2861c8e31e3f90884897c22e4482
Karanbir Toor <karan.toor@appfolio.com>
2014-09-24 11:43:55 -0700

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
 end
-
-# This is to disable JazzHands from changing your console.
-# unless you call "rails jhc"
-if ARGV[0] == 'jhc' || ARGV[0] == 'jazzhands_console'
-  ARGV[0] = "console"
-else
-  ENV['DISABLE_PRY_RAILS'] = 'true'
-end
